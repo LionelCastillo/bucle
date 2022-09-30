04857db5390839362ac0ff71a256073e369125c5
LionelCastillo <105450241+LionelCastillo@users.noreply.github.com>
2022-09-30 15:16:33 -0300

    Add files via upload

--- LAST_NAME_LoopDLoop.js
+++ LAST_NAME_LoopDLoop.js
@@ -2,21 +2,20 @@
 //2) El punto de partida es 0
 //3) El bucle debe detenerse cuando la variable no sea menor a 10 
 //4) Colocando en el bucle que se detenga al superar los 10
-//5) El incremento podria ser de tres en tres ya que se contaran cada tres kilometros, elegie hacerlo de a uno,
-// para que siempre evalue si la condicion se cumple  
+//5) El incremento es de tres en tres 
 //6) Las variables que necesitamos son "c" para contar los caramelos, "v" para indicar la velocidad del corredor, 
 // y dentro del bucle for "k" para contar los kilometros del corredor 
 var c = 0;
 var v = 12;
 
-for(var k = 0; k < 10; k++){
+for(var k = 0; k < 10; k+=3){
     if(k%3 == 0 && k !=0){
         c +=1
     }
 }
 
 //stretch 1
-for(var k = 0; k < 10; k++){
+for(var k = 0; k < 10; k+=3){
     if(k/3 == 1 && k !=0 && v > 9){
     c +=1
     }
